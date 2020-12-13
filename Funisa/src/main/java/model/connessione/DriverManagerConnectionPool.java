bcb33261a678b64d2a734cb81212c6800dc04d85
ndtefu <antonior1999a@gmail.com>
2020-12-13 21:21:23 +0100

    Progetto spostato su Intellij
    Intellij ha problemi con progetti creati su eclipse per via delle dipendenze

---
+++ Funisa/src/main/java/model/connessione/DriverManagerConnectionPool.java
@@ -0,0 +1,60 @@
+package model.connessione;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+public class DriverManagerConnectionPool  {
+
+    private static List<Connection> freeDbConnections= new LinkedList<Connection>();
+
+    static {
+        freeDbConnections = new LinkedList<Connection>();
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+    public DriverManagerConnectionPool() {
+        freeDbConnections = new LinkedList<Connection>();
+    }
+
+    private static synchronized Connection createDBConnection() throws SQLException {
+
+        String url="jdbc:sqlserver://funisa.database.windows.net:1433;database=funisa;user=ndtefu@funisa;password=PasswordSicura1!;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+        Connection newConnection = DriverManager.getConnection(url);
+        newConnection.setAutoCommit(false);
+        return newConnection;
+    }
+
+
+    public static synchronized Connection getConnection() throws SQLException {
+        Connection connection;
+
+        if (!freeDbConnections.isEmpty()) {
+            connection = (Connection) freeDbConnections.get(0);
+            freeDbConnections.remove(0);
+
+            try {
+                if (connection.isClosed())
+                    connection = getConnection();
+            } catch (SQLException e) {
+                connection.close();
+                connection = getConnection();
+            }
+        } else {
+            connection = createDBConnection();
+        }
+
+        return connection;
+    }
+
+    public static synchronized void releaseConnection(Connection connection) throws SQLException {
+        if(connection != null) freeDbConnections.add(connection);
+    }
+}
