d65ef287fd9ee23fd05d7590490da8d7c8b4b00a
ndtefu <antonior1999a@gmail.com>
2020-12-14 14:04:43 +0100

    Ritorno ad eclipse

---
+++ database/Funisa.sql
@@ -0,0 +1,101 @@
+/*
+drop table segnalazione
+drop table recensione
+drop table notifica_utente
+drop table prenotazione_periferica
+drop table notifica
+drop table periferica
+drop table prenotazione
+drop table postazione
+drop table utente
+drop table categoria
+*/
+
+CREATE TABLE notifica(
+    id INT PRIMARY KEY NOT NULL IDENTITY (1, 1),
+    descrizione VARCHAR(50) NOT NULL,
+    isRead BIT NOT NULL,
+    tipo VARCHAR(20)
+)
+
+CREATE TABLE utente(
+    email VARCHAR(30) PRIMARY KEY NOT NULL,
+    nome VARCHAR(30) NOT NULL,
+    cognome VARCHAR(30) NOT NULL,
+    username VARCHAR(30) NOT NULL,
+    passw BINARY(32) NOT NULL,
+    ruolo VARCHAR(10) NOT NULL,
+    stato BIT NOT NULL DEFAULT 'FALSE',
+    codiceVerifica char(7),
+)
+
+CREATE TABLE segnalazione(
+    id INT PRIMARY KEY NOT NULL IDENTITY (1, 1),
+    tipo VARCHAR(10) NOT NULL,
+    descrizione VARCHAR(100) NOT NULL,
+    utenteEmail VARCHAR(30) NOT NULL,
+    FOREIGN KEY (UtenteEmail) REFERENCES utente(email)
+)
+
+CREATE TABLE recensione(
+    descrizione VARCHAR(200) NOT NULL,
+    valutazione INT NOT NULL,
+    verificata bit NOT NULL DEFAULT 'FALSE',
+    utenteEmail VARCHAR(30) NOT NULL,
+    FOREIGN KEY (UtenteEmail) REFERENCES utente(email)
+)
+
+CREATE TABLE periferica(
+    tipo VARCHAR(20) NOT NULL,
+    nome VARCHAR(20) NOT NULL PRIMARY KEY,
+    quantita INTEGER NOT NULL,
+    prezzo FLOAT NOT NULL
+)
+CREATE TABLE categoria(
+    nome VARCHAR(15) PRIMARY KEY NOT NULL,
+    tipoGenerico VARCHAR(15) NOT NULL,
+    prezzo FLOAT NOT NULL,
+    descrizione VARCHAR(100) NOT NULL,
+    immagine VARCHAR(MAX) NOT NULL
+)
+CREATE TABLE postazione(
+    id INT PRIMARY KEY NOT NULL IDENTITY (1, 1),
+    nomeCategoria VARCHAR(15) NOT NULL,
+    disponibile BIT NOT NULL,
+    FOREIGN KEY (nomeCategoria) REFERENCES categoria(nome)
+)
+
+
+CREATE TABLE prenotazione(
+    id INT PRIMARY KEY NOT NULL IDENTITY (1, 1),
+    dataPrenotazione DATE NOT NULL,
+    fasciaOraria CHAR(5) NOT NULL,
+    qR VARCHAR(MAX),/*è un'immagine*/
+    utenteEmail VARCHAR(30) NOT NULL,
+    postazioneId INT NOT NULL,
+    FOREIGN KEY (postazioneid) REFERENCES postazione(id),
+    FOREIGN KEY (UtenteEmail) REFERENCES utente(email)
+)
+
+CREATE TABLE notifica_utente(
+    notificaId INT NOT NULL,
+    UtenteEmail VARCHAR(30) NOT NULL,
+    PRIMARY KEY(notificaId,UtenteEmail),
+    FOREIGN KEY (UtenteEmail) REFERENCES utente(email),
+    FOREIGN KEY (notificaId) REFERENCES notifica(id)
+)
+
+CREATE TABLE prenotazione_periferica(
+    prenotazioneId INT NOT NULL,
+    perifericaNome VARCHAR(20) NOT NULL,
+    PRIMARY KEY(prenotazioneId,perifericaNome),
+    FOREIGN KEY(prenotazioneId) REFERENCES prenotazione(id),
+    FOREIGN KEY(perifericaNome) REFERENCES periferica(nome) 
+)
+
+
+create table test(
+    nome VARCHAR(10)
+)
+
+insert into test VALUES("prova")
