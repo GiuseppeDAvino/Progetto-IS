09341ce0fc55c4ad0e8d2350b7da2198879b7ac5
gadf00 <g.defilippo22@studenti.unisa.it>
2020-12-19 19:03:19 +0100

    AggiungiPostazione

---
+++ src/control/AggiungiPostazione.java
@@ -0,0 +1,51 @@
+package control;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.postazione.PostazioneBean;
+import model.postazione.PostazioneDAO;
+
+/**
+ * Servlet implementation class AggiungiPostazione
+ */
+@WebServlet(urlPatterns = {"/AggiungiPostazione","/titolare/AggiungiPostazione"})
+public class AggiungiPostazione extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+    private PostazioneDAO postazioneDAO = new PostazioneDAO();   
+
+    public AggiungiPostazione() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PostazioneBean postazione = new PostazioneBean();
+		postazione.setCategoria(request.getParameter("categoria"));
+		try {
+		postazioneDAO.doSave(postazione);
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
